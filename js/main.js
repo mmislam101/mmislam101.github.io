2650144c7673717a1516aa66f46130a44defee5e
williamnewton <williamnewbton@gmail.com>
2015-03-16 21:02:35 -0700

    initial website splash complete

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$('input').click(function(){
+	$('.fa-envelope').addClass('active');
+});
+
+$(document).ready(function(){
+	setTimeout(function(){
+    	$('.masker').removeClass('masken');
+	}, 200);
+	setTimeout(function(){
+    	$('.mkt h1').removeClass('slidden');
+	}, 1500);
+	setTimeout(function(){
+    	$('.mkt p').removeClass('slidden');
+	}, 3500);
+	setTimeout(function(){
+    	$('.form-wrapper').removeClass('slidden');
+	}, 5000);
+
+});
